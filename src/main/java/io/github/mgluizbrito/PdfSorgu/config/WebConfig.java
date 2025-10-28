8cdfbab9d0683181a5bb638ec479cdd1ce830c81
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-10-28 15:27:58 -0300

    feat: ResourceHandler for documents mapping on uploads dir

---
+++ src/main/java/io/github/mgluizbrito/PdfSorgu/config/WebConfig.java
@@ -0,0 +1,23 @@
+package io.github.mgluizbrito.PdfSorgu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final String DOWNLOAD_DIR = "./uploads";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        Path downloadPath = Paths.get(DOWNLOAD_DIR);
+        String absolutePath = downloadPath.toFile().getAbsolutePath();
+
+        registry.addResourceHandler("/v1/files/**")
+                .addResourceLocations("file:" + absolutePath + "/");
+    }
+}
