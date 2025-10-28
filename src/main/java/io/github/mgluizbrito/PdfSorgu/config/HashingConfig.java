39d92ff40d5d8a300089d06006ed495e2b5a2959
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-10-28 17:45:19 -0300

    feat: SHA256 hashing implementation

---
+++ src/main/java/io/github/mgluizbrito/PdfSorgu/config/HashingConfig.java
@@ -0,0 +1,61 @@
+package io.github.mgluizbrito.PdfSorgu.config;
+
+import io.github.mgluizbrito.PdfSorgu.utils.HashAlgorithm;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Configuration
+public class HashingConfig {
+
+    @Bean
+    public HashAlgorithm hashEncoder() throws NoSuchAlgorithmException {
+        final String ALGORITHM = "SHA-256";
+        MessageDigest digest = MessageDigest.getInstance(ALGORITHM);
+
+        return new HashAlgorithm() {
+
+            @Override
+            public String msgHash(String msg) {
+                byte[] encodedhash = digest.digest(msg.getBytes(StandardCharsets.UTF_8));
+
+                StringBuilder sb = new StringBuilder();
+                for (byte b : encodedhash) {
+                    sb.append(String.format("%02x", b));
+                }
+
+                return sb.toString();
+            }
+
+            @Override
+            public String fileHash(File file) throws IOException {
+
+                try (FileInputStream fis = new FileInputStream(file)) {
+                    byte[] byteArray = new byte[8192]; // 8KB buffer
+                    int bytesCount;
+
+                    while ((bytesCount = fis.read(byteArray)) != -1) {
+                        // Atualiza o digest com cada pedaço
+                        digest.update(byteArray, 0, bytesCount);
+                    }
+                }
+                // Obter o hash final como um array de bytes
+                byte[] bytes = digest.digest();
+
+                // 5. Converter o array de bytes para uma string hexadecimal
+                StringBuilder sb = new StringBuilder();
+                for (byte b : bytes) {
+                    sb.append(String.format("%02x", b));
+                }
+
+                return sb.toString();
+            }
+        };
+    }
+}
---
+++ src/main/java/io/github/mgluizbrito/PdfSorgu/utils/HashAlgorithm.java
@@ -0,0 +1,38 @@
+package io.github.mgluizbrito.PdfSorgu.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Interface for generating cryptographic hash values (digests)
+ * for both strings (messages) and files, using various algorithms.
+ */
+public interface HashAlgorithm {
+
+    /**
+     * Generates the cryptographic hash (digest) for a given input message string.
+     * <p>
+     * The specific hashing algorithm used must be determined by the
+     * concrete implementation of this interface.
+     *
+     * @param msg The input string message to be hashed.
+     * @return The hash value as a hexadecimal string.
+     */
+    String msgHash(String msg);
+
+
+    /**
+     * Generates the cryptographic hash (digest) for the contents of a file.
+     * <p>
+     * This implementation reads the file in chunks (8KB buffer)
+     * to efficiently handle large files without excessive memory usage.
+     *
+     * @param file The {@code File} object representing the file to be hashed.
+     * @return The file's hash value as a hexadecimal string.
+     * @throws IOException If an I/O error occurs while reading the file.
+     * @throws NoSuchAlgorithmException If the specified algorithm is not
+     * available in the environment.
+     */
+     String fileHash(File file) throws IOException, NoSuchAlgorithmException;
+}
