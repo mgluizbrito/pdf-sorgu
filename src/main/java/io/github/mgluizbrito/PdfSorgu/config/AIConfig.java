9744649028bef1a80dc5cc8465daa99a1ae36b2e
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-11-01 16:01:22 -0300

    feat: adding multi-support for chat models based on application profiles and custom beans (SET PROFILE BY ARGS)

--- src/main/java/io/github/mgluizbrito/PdfSorgu/config/AIConfig.java
+++ src/main/java/io/github/mgluizbrito/PdfSorgu/config/AIConfig.java
@@ -5,12 +5,15 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.google.genai.GoogleGenAiChatModel;
 import org.springframework.ai.google.genai.GoogleGenAiChatOptions;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.ollama.OllamaEmbeddingModel;
 import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaChatOptions;
 import org.springframework.ai.ollama.api.OllamaEmbeddingOptions;
 import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -39,19 +42,39 @@ public class AIConfig {
     // OLLAMA (Para EmbeddingModel)
     // ==================================================================================
 
-    @Value("${spring.ai.ollama.base-url:http://localhost:11434}")
-    private String OLLAMA_BASE_URL;
-
-    @Value("${spring.ai.ollama.embedding.options.model:nomic-embed-text}")
-    private String embeddingModelName;
-
+    @ConditionalOnProperty(name = "spring.ai.ollama.base-url")
     @Bean
-    public OllamaApi ollamaApi() {
+    public OllamaApi ollamaApi(
+            @Value("${spring.ai.ollama.base-url:http://localhost:11434}") String OLLAMA_BASE_URL
+    ) {
+
         return OllamaApi.builder().baseUrl(OLLAMA_BASE_URL).build();
     }
 
+    @ConditionalOnProperty(name = "spring.ai.ollama.chat.options.model")
     @Bean
-    public EmbeddingModel embeddingModel(OllamaApi ollamaApi) {
+    public ChatModel ollamaChatModel(
+            OllamaApi ollamaApi,
+            @Value("${spring.ai.ollama.chat.options.model}") String modelName
+    ){
+
+        OllamaChatOptions options = OllamaChatOptions.builder()
+                .model(modelName)
+                .build();
+
+        return OllamaChatModel.builder()
+                .ollamaApi(ollamaApi)
+                .defaultOptions(options)
+                .build();
+    }
+
+    @ConditionalOnProperty(name = "spring.ai.ollama.base-url")
+    @Bean
+    public EmbeddingModel embeddingModel(
+            OllamaApi ollamaApi,
+            @Value("${spring.ai.ollama.embedding.options.model:nomic-embed-text}") String embeddingModelName
+    ) {
+
         OllamaEmbeddingOptions options = OllamaEmbeddingOptions.builder()
                 .model(embeddingModelName)
                 .build();
@@ -63,21 +86,24 @@ public class AIConfig {
     // GEMINI (Para ChatModel)
     // ==================================================================================
 
-    @Value("${spring.ai.google.genai.api-key}")
-    private String apiKey;
-
-    @Value("${spring.ai.google.genai.chat.options.model}")
-    private String modelName;
-
+    @ConditionalOnProperty(name = "spring.ai.google.genai.api-key")
     @Bean
-    public Client googleGenAiClient(){
+    public Client googleGenAiClient(
+            @Value("${spring.ai.google.genai.api-key}") String apiKey
+    ){
+
         return Client.builder()
                 .apiKey(apiKey)
                 .build();
     }
 
+    @ConditionalOnProperty(name = "spring.ai.google.genai.api-key")
     @Bean
-    public ChatModel googleGenAiChatModel(Client googleGenAiClient){
+    public ChatModel googleGenAiChatModel(
+            Client googleGenAiClient,
+            @Value("${spring.ai.google.genai.chat.options.model}") String modelName
+    ) {
+
         GoogleGenAiChatOptions options = GoogleGenAiChatOptions.builder()
                 .model(modelName)
                 .build();
